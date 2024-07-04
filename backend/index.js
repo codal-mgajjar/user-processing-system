600c77365d949d78851c62a4d2628adfe9f185e0
codal-mgajjar <mgajjar@codal.com>
2024-07-04 11:10:51 +0530

    changes done

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient();
+const params = {
+    TableName: 'UsersTable'
+}
+
+
+const listItems = async() => {
+    try {
+        const data = await docClient.scan(params).promise()
+        return data;
+    } catch(error) {
+        return error;
+    }
+}
+
+exports.handler = async (event, context) => {
+    try {
+        const data = await listItems();
+        return { body: JSON.stringify(data)}
+    } catch(error){
+        return { error:error }
+    }
+};  
+
+
+console.log("Helloo");
