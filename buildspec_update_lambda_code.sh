35ab65cc6f1a1934d528deacdc74cc74273f0192
codal-mgajjar <mgajjar@codal.com>
2024-07-09 17:45:14 +0530

    Lambda updated

--- buildspec_update_lambda_code.sh
+++ buildspec_update_lambda_code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mkdir -p ./deployment_zip 
-cp ./backend/index.js  ./deployment_zip
-cd deployment_zip 
 zip -r lambda_zip.zip *
-aws lambda update-function-code --function-name getALLUsers --zip-file fileb://./deployment_zip/lambda_zip.zip
+aws lambda update-function-code --function-name getALLUsers --zip-file fileb://lambda_zip.zip
